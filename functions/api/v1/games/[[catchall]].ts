699a0e37725450f4a34a121c105c6a979fb626e2
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-01-03 19:51:12 +0800

    Test /api/v1/games/*

--- functions/api/v1/games/[[catchall]].ts
+++ functions/api/v1/games/[[catchall]].ts
@@ -1,14 +1,16 @@
-export const onRequest: PagesFunction = async context => {
-  const [brand, state] = context.params.catchall
+export const onRequestGet: PagesFunction = async context => {
+  const params = context.params.catchall
+  const request = context.request
 
-  if (!brand || !state) {
+  if (!params || params.length !== 2) {
     return new Response(null, {
       status: 418,
       statusText: "I'm a teapot",
     })
   }
 
-  const data = JSON.stringify({ brand, state })
+  const [brand, state] = params
+  const data = JSON.stringify({ brand, state, request })
 
   return new Response(data, {
     headers: { 'Content-Type': 'application/json;charset=UTF-8' },
