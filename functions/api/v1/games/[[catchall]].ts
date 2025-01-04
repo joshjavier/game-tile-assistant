f11d2912a0a191f00175deca9e42d5167ba1e927
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-01-04 08:42:49 +0800

    Implement dynamic api route with data fetching

--- functions/api/v1/games/[[catchall]].ts
+++ functions/api/v1/games/[[catchall]].ts
@@ -1,6 +1,46 @@
+const brandSlugs = ['betmgm', 'borgata', 'wof', 'partycasino']
+const states = ['nj', 'pa', 'mi', 'wv', 'on']
+const brandDomain = {
+  betmgm: 'betmgm',
+  borgata: 'borgataonline',
+  partycasino: 'partycasino',
+  wof: 'wheeloffortunecasino',
+}
+
+const getCasinoBaseUrl = (
+  brandSlug = 'betmgm',
+  state = 'nj',
+  { gameSubdomain = false } = {},
+): string => {
+  if (!brandSlugs.includes(brandSlug) || !states.includes(state)) {
+    throw new Error('Invalid brand or state argument')
+  }
+
+  const brand = brandDomain[brandSlug]
+  const segments = {
+    product: gameSubdomain ? 'casinogames' : 'casino',
+    state,
+    brand,
+    tld: 'com',
+  }
+
+  // MC ON has a '.ca' top-level domain
+  if (brandSlug === 'betmgm' && state === 'on') {
+    segments.tld = 'ca'
+  }
+
+  // BC NJ and WOF NJ don't have a state subdomain
+  const isBCNJ = brandSlug === 'borgata' && state === 'nj'
+  const isWOFNJ = brandSlug === 'wof' && state === 'nj'
+  if (isBCNJ || isWOFNJ) {
+    segments.state = null
+  }
+
+  return `https://${Object.values(segments).filter(Boolean).join('.')}`
+}
+
 export const onRequestGet: PagesFunction = async context => {
   const params = context.params.catchall
-  const request = context.request
 
   if (!params || params.length !== 2) {
     return new Response(null, {
@@ -10,9 +50,29 @@ export const onRequestGet: PagesFunction = async context => {
   }
 
   const [brand, state] = params
-  const data = JSON.stringify({ brand, state, request })
+  const baseUrl = getCasinoBaseUrl(brand, state)
+  const apiUrl = new URL(
+    baseUrl + '/en/games/api/content/GetGameMetaDataFromLMTAsync',
+  )
+  const apiOrigin = apiUrl.origin
 
-  return new Response(data, {
-    headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+  const originHeader = { Origin: apiOrigin }
+  const mobileHeader = {
+    'User-Agent':
+      'Mozilla/5.0 (Android 13; Mobile; rv:109.0) Gecko/114.0 Firefox/114.0',
+  }
+
+  const dReq = new Request(apiUrl, { headers: originHeader })
+  const mReq = new Request(apiUrl, {
+    headers: { ...originHeader, ...mobileHeader },
   })
+
+  const responses = await Promise.all([fetch(dReq), fetch(mReq)])
+  const results = await Promise.all(responses.map(r => r.json()))
+
+  const options = {
+    headers: { 'content-type': 'application/json;charset=UTF-8' },
+  }
+
+  return new Response(JSON.stringify(results), options)
 }
