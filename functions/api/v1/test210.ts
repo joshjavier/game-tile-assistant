51934c1b1b950540dae5debd0ca6c475b348f6d4
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-01-04 09:58:40 +0800

    Test 210

---
+++ functions/api/v1/test210.ts
@@ -0,0 +1,28 @@
+export const onRequest: PagesFunction = async context => {
+  let { request } = context
+
+  if (
+    request.method === 'GET' ||
+    request.method === 'HEAD' ||
+    request.method === 'POST'
+  ) {
+    const url = new URL(request.url)
+    const apiUrl =
+      'https://casino.on.betmgm.ca/en/games/api/content/GetGameMetaDataFromLMTAsync'
+
+    request = new Request(apiUrl, request)
+    request.headers.set('Origin', new URL(apiUrl).origin)
+
+    let response = await fetch(request)
+    response = new Response(response.body, response)
+    response.headers.set('Access-Control-Allow-Origin', url.origin)
+    response.headers.append('Vary', 'Origin')
+
+    return response
+  } else {
+    return new Response(null, {
+      status: 405,
+      statusText: 'Method Not Allowed',
+    })
+  }
+}
